56c444336f7951d6be8e650bafa12d703c0ddd9a
Gabriel Rosa <143665401+GabrielRosa789@users.noreply.github.com>
2025-11-20 16:00:51 -0300

    Update order.py

--- src/models/order.py
+++ src/models/order.py
@@ -1,1 +1,21 @@
+from patterns.observer.subject import Subject
+
+
+class Order(Subject):
+    """
+    Pedido que pode mudar de status.
+    Ao mudar de status, notifica os observadores.
+    """
+
+    def __init__(self, order_id: str):
+        super().__init__()
+        self.order_id = order_id
+        self.status = "Criado"
+
+    def set_status(self, new_status: str):
+        """
+        Atualiza o status e notifica os observadores.
+        """
+        self.status = new_status
+        self.notify(f"Pedido {self.order_id} agora está: {self.status}")
 
