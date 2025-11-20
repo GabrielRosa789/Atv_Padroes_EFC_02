49e953c8cf87552c9d7a8e9348875f693bac8d78
Gabriel Rosa <143665401+GabrielRosa789@users.noreply.github.com>
2025-11-20 14:30:23 -0300

    Update fixed_discount.py

--- src/patterns/strategy/fixed_discount.py
+++ src/patterns/strategy/fixed_discount.py
@@ -1,1 +1,14 @@
+# src/patterns/strategy/fixed_discount.py
+from .discount_strategy import DiscountStrategy
+
+class FixedDiscount(DiscountStrategy):
+    """
+    Estratégia que aplica um valor fixo de desconto.
+    """
+
+    def __init__(self, value: float):
+        self.value = value
+
+    def calculate_discount(self, amount: float) -> float:
+        return min(self.value, amount)
 
