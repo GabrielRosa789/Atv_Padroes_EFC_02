a9d35ac1295a068a0886c6a71c31072b8dd31847
Gabriel Rosa <143665401+GabrielRosa789@users.noreply.github.com>
2025-11-20 16:11:09 -0300

    Refactor shopping cart to include amount in init

--- src/patterns/strategy/shopping_cart.py
+++ src/patterns/strategy/shopping_cart.py
@@ -5,20 +5,17 @@ class ShoppingCart:
     Carrinho que usa uma estratégia de desconto.
     """
 
-    def __init__(self, discount_strategy: DiscountStrategy):
+    def __init__(self, amount: float, discount_strategy: DiscountStrategy):
+        self.amount = amount
         self.discount_strategy = discount_strategy
 
     def set_discount_strategy(self, strategy: DiscountStrategy):
-        """
-        Permite trocar a estratégia dinamicamente.
-        """
         self.discount_strategy = strategy
 
-    def calculate_total(self, amount: float) -> float:
+    def calculate_total(self, amount: float = None) -> float:
         """
         Calcula o valor total aplicando a estratégia atual.
         """
+        amount = amount if amount is not None else self.amount
         discount = self.discount_strategy.calculate_discount(amount)
-        total = amount - discount
-        return total
-
+        return amount - discount
